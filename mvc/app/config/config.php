cacde906307d7941c20f85c0695100affbd9f068
iqbal nuryana <iqbalnuryana68@gmail.com>
2022-10-18 01:03:16 +0700

    desain

---
+++ mvc/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL','http://localhost/prakweb_C_203040148/mvc/public/');
+
+//DB
+define('DB_HOST','localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
